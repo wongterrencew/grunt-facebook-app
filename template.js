c88f7ca627299e99158cefc190e730cb3a271529
Terrence Wong <wongterrencew@gmail.com>
2016-08-19 18:43:20 +0200

    remove semi-colons

--- template.js
+++ template.js
@@ -6,19 +6,19 @@
  * Licensed under the MIT License
  */
 
-'use strict';
+'use strict'
 
 // Basic template description
-exports.description = 'Create a Facebook app.';
+exports.description = 'Create a Facebook app.'
 
 // Template-specific notes to be displayed before question prompts.
-exports.notes = '';
+exports.notes = ''
 
 // Template-specific notes to be displayed after the question prompts.
-exports.after = '';
+exports.after = ''
 
 // Any existing file or directory matching this wildcard will cause a warning.
-exports.warnOn = '*';
+exports.warnOn = '*'
 
 // The actual init template
 exports.template = function( grunt, init, done ) {
@@ -34,8 +34,8 @@ exports.template = function( grunt, init, done ) {
 			default: 'LESS'
 		}
 	], function( err, props ) {
-		props.keywords = [];
-		props.version = '0.1.0';
+		props.keywords = []
+		props.version = '0.1.0'
 		props.devDependencies = {
 			'grunt': '~0.4.1',
 			'matchdep': '~0.1.2',
@@ -51,50 +51,50 @@ exports.template = function( grunt, init, done ) {
 			'grunt-contrib-watch': '~0.2.0',
 			'grunt-contrib-copy' : '~0.4.1',
 			'grunt-include-replace' : '~2.0.0'
-		};
+		}
 		
 		// Sanitize names where we need to for PHP/JS
-		props.name = props.title.replace( /\s+/g, '-' ).toLowerCase();
+		props.name = props.title.replace( /\s+/g, '-' ).toLowerCase()
 		// An additional value, safe to use as a JavaScript identifier.
 		// An additional value that won't conflict with NodeUnit unit tests.
 
 		// Files to copy and process
-		var files = init.filesToCopy( props );
+		var files = init.filesToCopy( props )
 
 		switch( props.css_type.toLowerCase()[0] ) {
 			case 'l':
-				delete files[ 'src/styles/sass/app.scss'];
-				delete files[ 'src/styles/src/app.css' ];
+				delete files[ 'src/styles/sass/app.scss']
+				delete files[ 'src/styles/src/app.css' ]
 				
-				props.devDependencies["grunt-contrib-less"] = "~0.11.2";
-				props.css_type = 'less';
-				break;
+				props.devDependencies["grunt-contrib-less"] = "~0.11.2"
+				props.css_type = 'less'
+				break
 			case 'n':
 			case undefined:
-				delete files[ 'src/styles/less/app.less'];
-				delete files[ 'src/styles/sass/app.scss'];
+				delete files[ 'src/styles/less/app.less']
+				delete files[ 'src/styles/sass/app.scss']
 				
-				props.css_type = 'none';
-				break;
+				props.css_type = 'none'
+				break
 			// SASS is the default
 			default:
-				delete files[ 'src/styles/less/app.less'];
-				delete files[ 'src/styles/src/app.css' ];
+				delete files[ 'src/styles/less/app.less']
+				delete files[ 'src/styles/src/app.css' ]
 				
-				props.devDependencies["grunt-contrib-sass"] = "~0.7.3";
-				props.css_type = 'sass';
-				break;
+				props.devDependencies["grunt-contrib-sass"] = "~0.7.3"
+				props.css_type = 'sass'
+				break
 		}
 		
-		console.log( files );
+		console.log( files )
 		
 		// Actually copy and process files
-		init.copyAndProcess( files, props );
+		init.copyAndProcess( files, props )
 		
 		// Generate package.json file
-		init.writePackageJSON( 'package.json', props );
+		init.writePackageJSON( 'package.json', props )
 		
 		// Done!
-		done();
-	});
-};
+		done()
+	})
+}
